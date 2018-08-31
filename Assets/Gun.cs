eeaf8683b8f598ba2d18dc955ecc5c57ba1462c6
nicholaicascio <lordcohliani@gmail.com>
2018-08-31 16:52:34 -0600

    0.3
    gun now does a raycast and damages target which has health and can die

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -1,16 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Gun : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float weaponDamage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
 	}
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(weaponDamage);
+            }
+        }
+    }
 }
