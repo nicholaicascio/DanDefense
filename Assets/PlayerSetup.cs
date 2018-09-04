5912bd1f30138f9c0bf05ab7229ad8d8329ab6b6
Zach Wooding <zacharywooding@u.boisestate.edu>
2018-09-04 14:20:56 -0600

    Added multiplayer functionality, changed Player to RigidbodyFPSController Prefab, added script PlayerSetup

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componetsToDisable;
+
+    Camera sceneCamera;
+
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i <= componetsToDisable.Length; i++)
+            {
+                componetsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if(sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+	}
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+
+}
