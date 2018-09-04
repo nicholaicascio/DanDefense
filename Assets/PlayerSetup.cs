bbe5740d1c5eecd8ce2ef8501edf958a7808f8c4
zachwooding <zachwooding@gmail.com>
2018-09-04 00:08:40 -0600

    Added multiplayer functionalit via PlayerSetup script and  NetworkManager

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componetsToDisable;
+
+    Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer)
+        {
+            for(int i = 0;i<componetsToDisable.Length; i++)
+            {
+                componetsToDisable[i].enabled = false;
+            }
+        }else
+        {
+            mainCamera = Camera.main;
+            if(mainCamera != null)
+            {
+                mainCamera.gameObject.SetActive(false);
+            }
+            
+        }
+	}
+
+    void OnDisable()
+    {
+        if (mainCamera != null)
+        {
+            mainCamera.gameObject.SetActive(true);
+        }
+    }
+}
