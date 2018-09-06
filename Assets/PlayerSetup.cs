2c897a9c48494d693677eaf520b97b7f7f4918c4
Zach Wooding <zacharywooding@u.boisestate.edu>
2018-09-05 21:19:03 -0600

    fixed merge disaster, added multiplayer function, add animations to multiplayer

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componetsToDisable;
+    Camera sceneCamera;
+    Camera fps;
+  
+
+	// Use this for initialization
+	void Start () {
+       
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i < componetsToDisable.Length; i++)
+            {
+                componetsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+             sceneCamera = Camera.main;
+             fps = gameObject.GetComponentInChildren<Camera>();
+             if (sceneCamera != null && fps != null)
+             {
+                 sceneCamera.gameObject.SetActive(false);
+                 fps.gameObject.SetActive(true);
+             }
+        }
+	}
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+
+}
