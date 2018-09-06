a8982befdb4df8d1e08953b25c310419a17dc298
nicholaicascio <lordcohliani@gmail.com>
2018-09-05 20:03:35 -0600

    Revert "Merge branch 'Multiplayer'"
    This reverts commit ad584e7ec4e55748302bbb1828d0ee825fb7db11, reversing
    changes made to 241cad5dbca337c12d74980ba3f6275eae953a96.

---
+++ Assets/Gun.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public float weaponDamage = 10f;
+    public float range = 100f;
+    public ParticleSystem muzzleFlash;
+
+    public Camera fpsCam;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+	}
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(weaponDamage);
+            }
+        }
+    }
+}
