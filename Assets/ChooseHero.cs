ff2149c771d81a2641bb8f22ebf17e7754e6682f
nicholaicascio <lordcohliani@gmail.com>
2018-09-24 18:45:18 -0600

    allowing for selection of characters
    click a button before joining or hosting a game

---
+++ Assets/ChooseHero.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ChooseHero : MonoBehaviour
+{
+
+    //public GameObject characterSelect;
+    public GameObject btn1;
+    public GameObject btn2;
+
+    public void PickHero(int hero)
+    {
+        NetworkManager.singleton.GetComponent<NetworkCustom>().chosenCharacter = hero;
+        //characterSelect.SetActive(false);
+        btn1.SetActive(false);
+        btn2.SetActive(false);
+    }
+}
