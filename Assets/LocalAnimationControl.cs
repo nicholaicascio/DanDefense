828ace509be3c863bf77f434b4199a5a0d94ac93
nicholaicascio <lordcohliani@gmail.com>
2018-12-05 16:04:19 -0700

    zacks animations
    added zack's anim for the soldiers. host can't see animations

---
+++ Assets/LocalAnimationControl.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LocalAnimationControl : NetworkBehaviour {
+
+
+    public Animator anim;
+    public GameObject[] animationBodyParts;
+    //public Material invisible;
+
+    [SyncVar(hook = "OnAnimationStateChange")]
+    public string animState = "idle";
+
+     
+
+    void OnAnimationStateChange(string aString)
+    {
+        if (isLocalPlayer) return;
+        UpdateAnimation(aString);
+    }
+
+    void UpdateAnimation(string aString)
+    {
+        
+        //Debug.Log(animState);
+        if (animState == aString) return;
+        animState = aString;
+
+        if(animState == "idle")
+        {
+            anim.Play("idle");
+        }
+
+        if(animState == "shoot")
+        {
+            anim.SetBool("Shoot", true);
+        }
+        else
+        {
+            anim.SetBool("Shoot", false);
+        }
+
+        if (animState == "guard")
+        {
+            anim.SetBool("Guard", true);
+        }
+        else
+        {
+            anim.SetBool("Guard", false);
+        }
+
+        if (animState == "run")
+        {
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+
+    }
+
+    
+    
+
+    [Command]
+    public void CmdUpdateAnim(string aString)
+    {
+        UpdateAnimation(aString);
+    }
+
+    
+    
+
+
+   /* void Start () {
+        //anim = GetComponentInChildren<Animator>();
+        //anim.SetBool("Idle", true);
+
+        if (isLocalPlayer)
+        {
+            foreach(GameObject g in animationBodyParts)
+            {
+                SkinnedMeshRenderer[] m = g.GetComponentsInChildren<SkinnedMeshRenderer>();
+                Renderer[] r = g.GetComponentsInChildren<Renderer>();
+                foreach(SkinnedMeshRenderer matRend in m)
+                {
+                    //matRend.material = invisible;
+                }
+
+                foreach(Renderer rend in r)
+                {
+                   //rend.material = invisible;
+                }
+            }
+        }
+	}*/
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/ToonSoldierScriptv2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ToonSoldierScriptv2 : NetworkBehaviour
+{
+    Animator anim;
+    LocalAnimationControl lAnCtrl;
+
+    //int runStateHash = Animator.StringToHash("Base Layer.Run");
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponentInChildren<Animator>();
+        lAnCtrl = GetComponent<LocalAnimationControl>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        bool move = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+        
+        
+        bool sprint = Input.GetKey(KeyCode.LeftShift);
+        //anim.SetBool("Sprint", sprint);
+        bool guard = Input.GetKey(KeyCode.E);
+        //anim.SetBool("Guard", guard);
+        bool shoot = Input.GetButton("Fire1");
+        //anim.SetBool("Shoot", shoot);
+
+        if (move)
+        {
+            lAnCtrl.CmdUpdateAnim("run");
+        }
+        else if (shoot)
+        {
+            lAnCtrl.CmdUpdateAnim("shoot");
+        }
+        else if (guard)
+        {
+            lAnCtrl.CmdUpdateAnim("guard");
+        }
+        else if (sprint)
+        {
+            lAnCtrl.CmdUpdateAnim("sprint");
+        }
+        else
+        {
+            lAnCtrl.CmdUpdateAnim("idle");
+        }
+       
+
+        
+    }
+}
